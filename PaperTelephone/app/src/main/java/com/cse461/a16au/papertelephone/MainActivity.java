ec59f1d45f13b828aca0f37ad27e35a3196f5700
Sidd Gorti <Sidd Gorti>
2016-11-29 12:47:06 -0800

    Added some minor changes

--- PaperTelephone/app/src/main/java/com/cse461/a16au/papertelephone/MainActivity.java
+++ PaperTelephone/app/src/main/java/com/cse461/a16au/papertelephone/MainActivity.java
@@ -1,14 +1,9 @@
 package com.cse461.a16au.papertelephone;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
+
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+
 
 import java.lang.Override;
 
